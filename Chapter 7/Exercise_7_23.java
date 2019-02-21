b63575eb8a4f7d203e904a56358afd3e43528241
10006039 <10006039@ironschools.org>
2019-02-21 12:57:56 -0700

    Add files via upload
    Added Chapter 7 assignments

---
+++ Chapter 7/Exercise_7_23.java
@@ -0,0 +1,28 @@
+/*
+Author: Skyler Wisdom
+Date: 2-21-19
+*/
+
+
+import java.util.Scanner;
+
+class Exercise_7_23 {
+
+	public static void main(String[] args) {
+		boolean[] lockers = new boolean[100];
+		
+		for (int student = 1; student <= 100; student++) {
+			for(int k = (student-1); k < 100; k += student) {
+				lockers[k] = !lockers[k];
+			
+			}
+		}
+	
+		for (int i = 0; i < lockers.length; i++) {
+			if(lockers[i] == true) {
+				System.out.printf("L%d ", i + 1);
+			
+			}
+		}
+	}
+}
